95c1b9c48c55b4979543c0afda2af17452873383
Okv <113612516+okvokv@users.noreply.github.com>
2023-07-05 18:04:54 +0300

    Add files via upload

--- controllers/users.js
+++ controllers/users.js
@@ -80,7 +80,7 @@ function login(req, res, next) {
                 sameSite: 'lax', // разрешена передача с одного и с разных сайтов
                 secure: false, // разрешена предача по http и по https
               })
-                .send({ message: 'Авторизация успешна.' }).setHeader('Access-Control-Allow-Credentials', 'true');
+                .send({ message: 'Авторизация успешна.' })
               return;
             }
             next(new UnauthorizedError());
